6ba8bb652f5dfbd579f98a1d5a12b52e7f7fcf04
Rezwan Mahmud <rezwan.mahmud.007@gmail.com>
2025-06-30 23:28:35 +0600

    Chapter-3-DataType
    DataType Chapter-3//

---
+++ Chapter-3-DetaType/keywords.js
@@ -0,0 +1,36 @@
+//Camel case--//
+
+const favouriteFood = "Pudding";
+
+// case sensitive ki seta dekhtesi//
+const city = "Chittagong";
+const City = "Dhaka";
+console.log(city);
+console.log(City);
+
+//anotherOne
+const userName = "Johan";
+const username = "jovan";
+console.log(username);
+console.log(username);
+
+// basar thikana with camel case
+const myHomeAddress = "Bamnachara";
+
+// error khabo sure because js er reserve keyword use korboo//
+const super = 23;
+console.log(super);
+// problem ache, its not allowed!
+
+// another error maybe
+const class = 12;
+console.log(class);
+// Its not allowed!
+
+// another error must!
+const 2ndPosition = "Sangakara";
+console.log(2ndPosition);
+// Error.
+
+
+
