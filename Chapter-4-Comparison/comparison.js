7e279f7f1c59146938bd197bc36a3fab36d5010f
Rezwan Mahmud <rezwan.mahmud.007@gmail.com>
2025-07-01 21:23:25 +0600

    Chapter-4-Comparison
    Nothing to add.

---
+++ Chapter-4-Comparison/comparison.js
@@ -0,0 +1,124 @@
+// practice 1-7 page-68
+
+const meAteSand = 7;
+const myBrotherSand = 5;
+const whoAteMore = (meAteSand > myBrotherSand);
+console.log(whoAteMore);
+
+//2nd
+const inMyClass = 45;
+const besideClass = 50;
+console.log(inMyClass < besideClass);
+
+//3rd
+const myNum = 75;
+const friNum = 75;
+console.log(myNum == friNum);
+
+//4th 
+const tkJomanoTarget = 100;
+const tkJomaichi = 95;
+console.log(tkJomanoTarget >= tkJomaichi);
+
+//5th
+console.log(10 != 7);
+
+//6th
+console.log(20<=15);
+
+//7th
+const myNumber = 45;
+const friendNum = 97;
+console.log(myNumber <= friendNum);
+
+
+//practice 1-8 page-70-71
+
+//1st
+const youngBroAge = 10;
+const myAge = 15;
+console.log(youngBroAge <= myAge);
+
+//2nd
+const passMark =33;
+const myMark = 45;
+console.log(passMark > myMark);
+//false, because ami pass korchi!
+
+//3rd
+const jamvura = 30;
+const kodbel = 40;
+console.log(jamvura < kodbel);
+
+//4th
+const iReadBooks = 10;
+const myFriRead = 10;
+console.log(iReadBooks == myFriRead);
+
+//5th
+const amarBiscuit = 15;
+const bondhurAche = 20;
+console.log(amarBiscuit<bondhurAche);
+
+//6th
+const realGoal = 2;
+const barsaGoal = 3;
+console.log(realGoal < barsaGoal);
+
+//7th
+const myName = "Rahim";
+const friName = "Karim";
+console.log(myName == friName);
+
+//8th
+const iReading = true;
+const myFrndReading = false;
+console.log(iReading == myFrndReading);
+
+
+
+//practice 1-7 page-73
+
+//1st
+const tkAche = 50;
+const bondhurKache = "50";
+console.log( tkAche == bondhurKache);
+console.log(tkAche === "50");
+//1st console - true, karon maan check korche just
+// 2nd console - false bcz, maan er sathe data type o check korche
+
+//2nd
+console.log("javascript" == 'javascript');
+console.log("javascript" === 'javascript');
+// 2tai true asbe. because 2tai string
+
+//3rd
+console.log(25 == "25");
+console.log(25 ==="25");
+//1st console true and 2nd one is false. bcz, 1st er tay shudhu maan dekhche and 2nd tay maan plus data o dekhche, son number and string to ar same hoite paree na ,tai false.
+
+//4th
+console.log("apple" == "apple");
+console.log( "apple" === "apple");
+//2tai true, because 2tai string!!
+
+//5th
+console.log("test" == "TEST");
+console.log("test" === "TEST");
+ //2tai false. upercase and lowercase issue. so 2ta same na.
+
+ //6th
+ const variableX = 15;
+ const variableY = "20";
+ console.log(variableX != variableY);
+ console.log(variableX !== variableY);
+ //2tai true
+
+ //7th
+ const variableA = hello;
+ const variableB = "Hello";
+ console.log(variableA == variableB);
+ console.log(variableA === variableB);
+ //error asbeee.
+
+
